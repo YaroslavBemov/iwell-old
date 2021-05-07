a5fac5a553832efc6933f2010c272f0375ac2ab0
Elena Ditiateva <elena2091@bk.ru>
2021-05-08 01:24:14 +0300

    added Routes in App

---
+++ resources/react-app/src/components/Classes/Classes.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Classes = () => {
+  return (
+    <div>
+      <h1>Example</h1>
+      <h3>lorem lorem lorem lorem lorem lorem lorem</h3>
+    </div>
+  );
+};
+
+export default Classes;
