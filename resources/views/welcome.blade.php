ba3bda51f91e7c5811dd04c174c587f8bad112a5
Elena Ditiateva <elena2091@bk.ru>
2021-05-09 16:05:23 +0300

    renamed app.css --> index.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{--Style--}}
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/index.css') }}" rel="stylesheet">
     <title>Laravel</title>
 </head>
 <body>
