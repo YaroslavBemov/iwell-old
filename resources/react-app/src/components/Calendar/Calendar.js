8bf8d39d6d534b2a661235099a2ab308b224415f
Elena Ditiateva <elena2091@bk.ru>
2021-05-12 22:54:38 +0300

    added interaction and customized styles

--- resources/react-app/src/components/Calendar/Calendar.js
+++ resources/react-app/src/components/Calendar/Calendar.js
@@ -1,30 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
-//import "./calendar.scss";
+import interactionPlugin from '@fullcalendar/interaction'
+import "./calendar.scss";
+
+import { INITIAL_EVENTS, createEventId } from './event-utils'
 
 const Calendar = () => {
-  const headerToolbar = {start: 'today prev,next', center: 'title', end: ''};
-  const buttonText = {today: 'Сегодня'}
+
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt('Please enter a new title for your event')
+    let calendarApi = selectInfo.view.calendar
+
+    calendarApi.unselect() // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      })
+    }
+  }
+
+  const renderEventContent = (eventInfo) => {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    )
+  }
+
+  const handleEventClick = (clickInfo) => {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove()
+    }
+  }
+
+  // const handleEvents = (event) => {
+  //   const newEvent = [...currentEvents, event.id];
+  //   setCurrentEvents(newEvent);
+  // }
+
   return (
     <div>
       <FullCalendar
-        plugins={[ timeGridPlugin ]}
+        plugins={[timeGridPlugin, interactionPlugin]}
         initialView="timeGridWeek"
+        headerToolbar={{ start: "today prev,next", center: "title", end: "" }}
         locale="ru"
         firstDay="1"
         slotMinTime="09:00:00"
         slotMaxTime="23:00:00"
-        slotDuration='00:60:00'
+        slotDuration="00:60:00"
         expandRows="true"
         height="700"
-        headerToolbar={headerToolbar}
-        buttonText={buttonText}
-        events={[
-          { title: 'event 1', date: '2021-05-11' },
-          { title: 'event 2', date: '2021-05-12' }
-        ]}
-      />  
+        buttonText={{ today: "Сегодня" }}
+        // events={[
+        //   { title: "event 1", date: "2021-05-11" },
+        //   { title: "event 2", date: "2021-05-12" },
+        // ]}
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+        initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+        select={handleDateSelect}
+        eventContent={renderEventContent} // custom render function
+        eventClick={handleEventClick}
+        //eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+      />
     </div>
   );
 };
