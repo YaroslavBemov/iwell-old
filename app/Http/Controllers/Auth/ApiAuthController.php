8522126af6dc4047d4ccf884b1aa0d79c183433a
Yaroslav <yaroslav.bemov@gmail.com>
2021-05-12 23:11:51 +0300

    add validator

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -3,30 +3,21 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\RegisterFormRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class ApiAuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterFormRequest $request)
     {
-        //TODO validator
-        $validator = Validator::make($request->all(), [
-            'first_name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
-        if ($validator->fails()) {
-            return response(['errors' => $validator->errors()->all()], 422);
-        }
-
         $user = User::create(array_merge(
             $request->all(),
             ['password' => bcrypt($request->password)]
         ));
-
         $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+        //TODO correct response
         $response = [
             'token' => $token,
             $user
@@ -51,6 +42,7 @@ class ApiAuthController extends Controller
         if (auth()->attempt($credentials)) {
             $token = auth()->user()->createToken('LaravelAuthApp')->accessToken;
             $user = auth()->user();
+            //TODO correct response
             $response = [
                 'token' => $token,
                 $user
--- app/Http/Requests/Api/RegisterFormRequest.php
+++ app/Http/Requests/Api/RegisterFormRequest.php
@@ -24,9 +24,15 @@ class RegisterFormRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6'],
+            'remember_token' => ['nullable', 'boolean'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'birthday' => ['nullable', 'date_format:Y-m-d'],
+            'gender' => ['nullable', 'in:male,female'],
+            'avatar' => ['nullable', 'image', 'max:50', 'dimensions:min_width=100,max_width=200,min_height=100,min_height=200'],
+//            'hometown_id' => ['required', 'exists:cities'],
         ];
     }
 }
