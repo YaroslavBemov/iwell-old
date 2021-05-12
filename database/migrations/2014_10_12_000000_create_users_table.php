b5e4b92bdece1e78a0226d3174989805cabacb8b
Yaroslav Bemov <yaroslav.bemov@gmail.com>
2021-05-12 12:58:27 +0300

    install passport with requires config

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password_hash');
             $table->rememberToken();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('birthday');
+            $table->string('gender');
+            $table->string('avatar');
+            $table->unsignedSmallInteger('role_id')->default(0);
+            $table->unsignedInteger('hometown_id');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
