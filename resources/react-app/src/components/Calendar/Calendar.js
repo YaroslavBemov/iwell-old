eb28f907d8250b7c3ef1007a9fae788ca4a39198
Elena Ditiateva <elena2091@bk.ru>
2021-05-15 21:41:31 +0300

    fix Calendar component

--- resources/react-app/src/components/Calendar/Calendar.js
+++ resources/react-app/src/components/Calendar/Calendar.js
@@ -11,21 +11,24 @@ const Calendar = () => {
   const [currentEvents, setCurrentEvents] = useState([]);
 
   const handleDateSelect = (selectInfo) => {
-    let title = prompt('Please enter a new title for your event')
+    //let title = prompt('Please enter a new title for your event')
     let calendarApi = selectInfo.view.calendar
+    console.log(calendarApi)
+    
+    //calendarApi.unselect() // clear date selection
 
-    calendarApi.unselect() // clear date selection
 
-    if (title) {
-      calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: false,
-        //display: "background",
-        url: '/classes',  //event.id
-      })
+    if (calendarApi) {
+      console.log("+++")
+      // calendarApi.addEvent({
+      //   id: createEventId(),
+      //   title,
+      //   start: selectInfo.startStr,
+      //   end: selectInfo.endStr,
+      //   allDay: false,
+      //   //display: "background",
+      //   url: '/classes',  //event.id
+      // })
     }
   }
 
@@ -40,7 +43,8 @@ const Calendar = () => {
 
   const handleEventClick = (clickInfo) => {
     if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove()
+      //clickInfo.event.remove()
+      console.log("click");
     }
   }
 
@@ -49,24 +53,27 @@ const Calendar = () => {
   //   setCurrentEvents(newEvent);
   // }
 
+  let events = [
+    { title: "event 1", date: "2021-05-13T17:00:00", display: "background" },
+    { title: "event 2", date: "2021-05-12T15:00:00", display: "background" },
+  ];
+
   return (
     <div>
       <FullCalendar
-        plugins={[timeGridPlugin, interactionPlugin]}
+        plugins={[ timeGridPlugin, interactionPlugin ]}
         initialView="timeGridWeek"
         headerToolbar={{ start: "today prev,next", center: "title", end: "" }}
         locale="ru"
         firstDay="1"
+        allDaySlot={false}
         slotMinTime="09:00:00"
         slotMaxTime="23:00:00"
         slotDuration="00:60:00"
-        expandRows="true"
-        height="700"
+        expandRows={true}
+        height="600"
         buttonText={{ today: "Сегодня" }}
-        // events={[
-        //   { title: "event 1", date: "2021-05-11" },
-        //   { title: "event 2", date: "2021-05-12" },
-        // ]}
+        events={events}
         editable={true}
         selectable={true}
         selectMirror={true}
@@ -76,7 +83,13 @@ const Calendar = () => {
         eventContent={renderEventContent} // custom render function
         eventClick={handleEventClick}
         //eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+
+      
       />
+      <div className="flex justify-end mt-40">
+        <span>Не больше 1 заказа</span>
+        <button className="button">Продолжить</button>
+      </div>
     </div>
   );
 };
