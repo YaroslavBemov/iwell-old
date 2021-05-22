73f4dad147dc305dc7f59f1bdb8fce33a2ca1c32
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-05-22 21:43:23 +0300

    login form refactored, added react-hook-form

--- resources/react-app/src/components/Login/Login.js
+++ resources/react-app/src/components/Login/Login.js
@@ -1,72 +1,85 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 import "./Login.scss";
 import CloseButton from "./icons/CloseButton";
-import LoginEmailStep from "./Steps/LoginEmailStep";
-import LoginPasswordStep from "./Steps/LoginPasswordStep";
-import LoginPayStep from "./Steps/LoginPayStep";
+import FacebookIcon from "./icons/FacebookIcon";
+import VkIcon from "./icons/VkIcon";
+import GoogleIcon from "./icons/GoogleIcon";
 
 const Login = ({ isOpened, closeLogin }) => {
-    const [formState, setFormState] = useState({
-        currentStep: 1,
-        username: "",
-        email: "",
-        password: "",
-        phone: "",
-    });
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-    };
+    const handleChange = (event) => event.target.value;
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(formState);
-    };
-
-    const nextStep = () => {
-        let currentStep = formState.currentStep;
-        currentStep = currentStep >= 2 ? 3 : currentStep + 1;
-        setFormState({
-            ...formState,
-            currentStep: currentStep,
-        });
-    };
+    const onSubmit = data => console.log(data);
 
     return (
         <div className={`login ${isOpened ? "open" : "close"}`}>
             <button className="login__close-btn" onClick={closeLogin}>
                 <CloseButton />
             </button>
-            <form className="login__wrapper" onSubmit={handleSubmit}>
-                <LoginEmailStep
-                    currentStep={formState.currentStep}
-                    handleChange={handleChange}
-                    email={formState.email}
-                >
-                    <button className="login__form-btn mt-40" onClick={nextStep}>
-                        Продолжить
-                    </button>
-                </LoginEmailStep>
-                <LoginPasswordStep
-                    currentStep={formState.currentStep}
-                    handleChange={handleChange}
-                    password={formState.password}
-                >
-                    <button className="login__form-btn mt-50" onClick={nextStep}>Войти</button>
-                </LoginPasswordStep>
-                <LoginPayStep
-                    currentStep={formState.currentStep}
-                    handleChange={handleChange}
-                    username={formState.username}
-                    email={formState.email}
-                    phone={formState.phone}
-                >
-                    <button className="login__form-btn mt-50">Оплатить</button>
-                </LoginPayStep>
+            <form className="login__wrapper" onSubmit={handleSubmit(onSubmit)}>
+                <h1 className="login__title">Вход</h1>
+                <div className="login__logo-wrapper mt-70">
+                    <a
+                        className="login__logo-facebook"
+                        href="https://www.facebook.com/"
+                        target="_blanc"
+                    >
+                        <FacebookIcon />
+                    </a>
+                    <a
+                        className="login__logo-vk"
+                        href="https://vk.com/"
+                        target="_blanc"
+                    >
+                        <VkIcon />
+                    </a>
+                    <a
+                        className="login__logo-google"
+                        href="https://google.com/"
+                        target="_blanc"
+                    >
+                        <GoogleIcon />
+                    </a>
+                </div>
+                <p className="login__text mt-70">или</p>
+                <input
+                    {...register("email", { required: true, pattern: /.+@.+\..+/i })}
+                    name="email"
+                    className="login__form-input mt-20"
+                    type="email"
+                    placeholder="Введите свой E-mail"
+                    onChange={handleChange}
+                />
+                {errors.email && <p className="login__errors-message mt-10">Введите Ваш Email</p>}
+                <input
+                    {...register("password", { required: true, minLength: 6 })}
+                    name="password"
+                    className="login__form-input mt-20"
+                    type="password"
+                    placeholder="Введите пароль"
+                    onChange={handleChange}
+                />
+                {errors.password && <p className="login__errors-message mt-10">Введите Ваш пароль - минимум 6 символов</p>}
+                <button className="login__form-btn mt-40" type="submit">
+                    Войти
+                </button>
+                <p className="login__small-text mt-20">
+                    Продолжая, Вы соглашаетесь с{" "}
+                    <Link className="login__link" to="/conditions">
+                        Условиями
+                    </Link>{" "}
+                    и{" "}
+                    <Link className="login__link" to="/privacy_policy">
+                        Политикой конфиденциальности
+                    </Link>
+                </p>
             </form>
         </div>
     );
