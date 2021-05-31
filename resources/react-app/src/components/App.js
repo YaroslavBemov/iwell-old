15557a8811cfb384dae31c0267578ed7c7e3014e
Elena Ditiateva <elena2091@bk.ru>
2021-05-31 22:30:44 +0300

    delete double React.StrictMode

--- resources/react-app/src/components/App.js
+++ resources/react-app/src/components/App.js
@@ -11,27 +11,25 @@ import ViewEvent from "./Calendar/viewEvent";
 
 function App() {
   return (
-    <React.StrictMode>
-      <Router>
-        <Header />
-        <Navbar />
-        <Content>
-          <Switch>
-            <Route exact path="/" component={Classes}/>
-            <Route path="/calendar" component={Calendar}/>
-            <Route path="/choice" component={BookEvent}/>
-            <Route path="/view_event" component={ViewEvent}/>
-            {/* <Route path="/subscription" component={Subscription}/>
-            <Route path="/info" component={Info}/>
-            <Route path="/trainings" component={Trainings}/>
-            <Route path="/tariffs" component={Tariffs}/>
-            <Route path="/schedule" component={Schedule}/>
-            <Route path="/clients" component={Clients}/> 
-            <Route path="/login" component={Login}/>*/}
-          </Switch>
-        </Content>
-      </Router>
-    </React.StrictMode>
+    <Router>
+      <Header />
+      <Navbar />
+      <Content>
+        <Switch>
+          <Route exact path="/" component={Classes}/>
+          <Route path="/calendar" component={Calendar}/>
+          <Route path="/choice" component={BookEvent}/>
+          <Route path="/view_event" component={ViewEvent}/>
+          {/* <Route path="/subscription" component={Subscription}/>
+          <Route path="/info" component={Info}/>
+          <Route path="/trainings" component={Trainings}/>
+          <Route path="/tariffs" component={Tariffs}/>
+          <Route path="/schedule" component={Schedule}/>
+          <Route path="/clients" component={Clients}/> 
+          <Route path="/login" component={Login}/>*/}
+        </Switch>
+      </Content>
+    </Router>
   );
 }
 
